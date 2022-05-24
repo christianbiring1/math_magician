d8581662fde84393b07e60312dec6e9725c6d88a
christian <christianbiringanine22@gmail.com>
2022-05-24 17:30:02 +0200

    Add changes required by the reviewer

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
-import './App.css';
+import React, { Component } from 'react';
+import Calculator from './components/Calculator';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { };
+  }
+
+  render() {
+    return (<Calculator />);
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Calculator from './components/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <App />
   </React.StrictMode>,
 );
 
