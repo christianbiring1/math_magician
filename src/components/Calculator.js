7554b25f9d45a8430b1b446a63fb946f0b2f3b5a
christian <christianbiringanine22@gmail.com>
2022-05-25 21:42:54 +0200

    Update calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -13,24 +13,24 @@ class Calculator extends Component {
         <div className="app-screen">0</div>
         <div className="app-input">
           <button type="submit" name="AC">AC</button>
-          <button type="submit">+/-</button>
-          <button type="submit">%</button>
-          <button type="submit" className="app-operator">÷</button>
-          <button type="submit">7</button>
-          <button type="submit">8</button>
-          <button type="submit">9</button>
-          <button type="submit" className="app-operator">x</button>
-          <button type="submit">4</button>
-          <button type="submit">5</button>
-          <button type="submit">6</button>
-          <button type="submit" className="app-operator">-</button>
-          <button type="submit">1</button>
-          <button type="submit">2</button>
-          <button type="submit">3</button>
-          <button type="submit" className="app-operator">+</button>
-          <button type="submit" className="double-item">0</button>
-          <button type="submit">.</button>
-          <button type="submit" className="app-operator">=</button>
+          <button type="submit" name="+/-">+/-</button>
+          <button type="submit" name="%">%</button>
+          <button type="submit" name="÷" className="app-operator">÷</button>
+          <button type="submit" name="7">7</button>
+          <button type="submit" name="8">8</button>
+          <button type="submit" name="9">9</button>
+          <button type="submit" name="x" className="app-operator">x</button>
+          <button type="submit" name="4">4</button>
+          <button type="submit" name="5">5</button>
+          <button type="submit" name="6">6</button>
+          <button type="submit" name="-" className="app-operator">-</button>
+          <button type="submit" name="1">1</button>
+          <button type="submit" name="2">2</button>
+          <button type="submit" name="3">3</button>
+          <button type="submit" name="+" className="app-operator">+</button>
+          <button type="submit" name="0" className="double-item">0</button>
+          <button type="submit" name=".">.</button>
+          <button type="submit" name="=" className="app-operator">=</button>
         </div>
       </div>
     );
